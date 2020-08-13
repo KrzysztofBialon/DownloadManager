3aa63b44eae99ce5865223ce4aa445647a94542a
unknown <kbialo12@gmail.com>
2020-08-13 13:45:05 +0200

    before_changing

--- src/sample/controllers/Controller.java
+++ src/sample/controllers/Controller.java
@@ -4,6 +4,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import sample.gui.DownloadItemBar;
@@ -13,10 +14,14 @@ public class Controller {
 
     @FXML
     private Button startDownloadBtn;
+    @FXML
+    private MenuButton extensionBtn;
+    @FXML
+    private TextField urlInputField;
 
     private ObservableList<Button> downloadsItemsObservableList;
 
-    String fileUrl = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=242990_a4634525489241b9a9e1aa73d9e118e6";
+    //String fileUrl = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=242990_a4634525489241b9a9e1aa73d9e118e6";
 
 
     @FXML
@@ -25,10 +30,11 @@ public class Controller {
         startDownloadBtn.addEventHandler(MouseEvent.MOUSE_CLICKED,
                                         mouseEvent ->
                                         {
-                                            try {
-
-                                                new HTTPConnectionClass(this.fileUrl).setConnection();
-                                            } catch (Exception e) {
+                                            try
+                                            {
+                                                new HTTPConnectionClass(this.urlInputField.getText()).setConnection();
+                                            } catch (Exception e)
+                                            {
                                                 e.printStackTrace();
                                             }
                                         });
